22f189a52eb1aa2d440f07bcd554452a0f51e85a
Ross Atkinson <ross.atkinson@gl-education.com>
2023-11-09 12:45:17 +0000

    Convert to PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,7 @@ import { UserLoginComponent } from './components/user-login/user-login.component
 import { RecoverResponseComponent } from './components/recover-response/recover-response.component';
 import { AppComponent } from './app.component';
 import { ResponseDetailComponent } from './components/response-detail/response-detail.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,13 @@ import { ResponseDetailComponent } from './components/response-detail/response-d
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
